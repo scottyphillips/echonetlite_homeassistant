7446fa2e9098656435e4f495e998f13b174abf76
Scott Phillips <scotty.phillips@hotmail.com>
2021-07-16 22:51:39 +1000

    partial support for generic ECHONETlite sensors!

--- custom_components/echonetlite/__init__.py
+++ custom_components/echonetlite/__init__.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 import logging
 import pychonet as echonet
+from pychonet.lib.epc import EPC_SUPER
 from datetime import timedelta
 
 _LOGGER = logging.getLogger(__name__)
@@ -17,17 +18,19 @@ MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=60)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    # TODO - fix this up to seletive configure API depending on entities.
+    # TODO - fix this up to seletive configure API depending on entities. 
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN].update({entry.entry_id: []})
+    _LOGGER.debug("platform entry data - %s", entry.data)
     for instance in entry.data["instances"]:
         # if ECHONETLite instance is HomeAirConditioner enable climate platform
         echonetlite = None
         if instance['eojgc'] == 1 and instance['eojcc'] == 48:
-            echonetlite = EchonetHVACAPIConnector(entry.data["host"])
+            echonetlite = HVACConnector(entry.data["host"])
+        else:
+            echonetlite = ECHONETConnector(instance['eojgc'],instance['eojcc'], instance['eojci'], entry.data["host"] )
         hass.data[DOMAIN][entry.entry_id].append({"instance_data": instance, "API":echonetlite})
-    # _LOGGER.debug(hass.data[DOMAIN])
-    # _LOGGER.debug("does config data get smushed? %s", entry.data["instances"])
+        
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
     return True
 
@@ -40,24 +43,53 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     return unload_ok
 
-"""EchonetAPIConnector is used to centralise API calls per platform
-   At some stage this will need to be refactored or extended to use the generic EchonetInstance class"""
-class EchonetHVACAPIConnector():
+"""EchonetHVACAPIConnector is used to centralise API calls for climate entities"""
+class HVACConnector():
     def __init__(self, host):
-       self._update_flags = [0x80, 0xB3, 0xA0, 0xA1, 0xA3, 0xA4, 0xA5, 0xB0, 0xBB, 0xBE] # outdoor temperature
+       self._update_flags = [0x80, 0xB3, 0xA0, 0xA1, 0xA3, 0xA4, 0xA5, 0xB0, 0xBB, 0xBE]
        self._update_data = {'status': 'Off'}
        self._api = echonet.HomeAirConditioner(host)
        self._update_data = self._api.update(self._update_flags)
 
-       # TODO - occasional bug here if ECHONETLite node doesnt return ID.
+       # TODO - occasional bug here if ECHONETLite node doesnt return ID. 
        try:
           self._uid = self._api.getIdentificationNumber()
        except IndexError:
           self._uid = f"{host}-{self._api.eojgc}-{self._api.eojcc}-{self._api.instance}"
           
-    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)   
+    async def async_update(self, **kwargs):
+        _LOGGER.debug("Commence polling ECHONET HVAC Instance")
+        self._update_data = self._api.update(self._update_flags)
+        _LOGGER.debug(f"polling ECHONET HVAC Instance complete - {self._update_data}")
+        return self._update_data
+
+"""EchonetAPIConnector is used to centralise API calls for generic Echonet devicess"""
+class ECHONETConnector():
+    def __init__(self, eojgc, eojcc, eojci, host):
+       _LOGGER.debug("initialisating generic ECHONET connector %s %s %s %s", eojgc, eojcc, eojci, host)    
+       self._update_data = {'status': 'Off'}
+       self._api = echonet.EchonetInstance(eojgc, eojcc, eojci, host)
+       _LOGGER.debug(f"what is this list giving me? {self._api.propertyMaps[0x9f].values()}")
+       self._useful_attributes = list(self._api.propertyMaps[0x9f].values())
+       for item in list(EPC_SUPER.keys()):
+            if item in self._useful_attributes:
+                self._useful_attributes.remove(item)
+       self._useful_attributes.append(0x80)
+       self._useful_attributes.append(0x84)
+       self._useful_attributes.append(0x85)
+       self._update_data = self._api.update(self._useful_attributes)
+
+       # TODO - occasional bug here if ECHONETLite node doesnt return ID. 
+       try:
+          self._uid = self._api.getIdentificationNumber()
+       except IndexError:
+          self._uid = f"{host}-{self._api.eojgc}-{self._api.eojcc}-{self._api.instance}"
+          
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)   
     async def async_update(self, **kwargs):
         _LOGGER.debug("Commence polling ECHONET Instance")
-        self._update_data = self._api.update(self._update_flags)
+        self._update_data = self._api.update(self._useful_attributes)
         _LOGGER.debug(f"polling ECHONET Instance complete - {self._update_data}")
         return self._update_data
+    
--- custom_components/echonetlite/sensor.py
+++ custom_components/echonetlite/sensor.py
@@ -4,6 +4,8 @@ import logging
 from homeassistant.const import CONF_ICON, CONF_NAME, CONF_TYPE, CONF_IP_ADDRESS, CONF_SENSORS
 from homeassistant.helpers.entity import Entity
 from homeassistant.util.unit_system import UnitSystem
+from pychonet.lib.epc import EPC_CODE 
+from pychonet.lib.eojx import EOJX_CLASS
 
 from .const import (
     DOMAIN,
@@ -17,10 +19,15 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(hass, config, async_add_entities, discovery_info=None):
     entities = []
     for entity in hass.data[DOMAIN][config.entry_id]:
+        _LOGGER.debug("processing entity %s")
         if entity['instance_data']['eojgc'] == 1 and entity['instance_data']['eojcc'] == 48: #Home Air Conditioner
             for op_code in HVAC_SENSOR_OP_CODES.keys():
                 if op_code in list(entity['API']._api.propertyMaps[159].values()):
                     entities.append(EchonetClimateSensor(entity['API'], op_code, HVAC_SENSOR_OP_CODES[op_code], hass.config.units, config.data["title"]))
+        else: #Generic Echonet
+            for op_code in EPC_CODE[entity['instance_data']['eojgc']][entity['instance_data']['eojcc']]:
+                if op_code in list(entity['API']._api.propertyMaps[159].values()):
+                    entities.append(EchonetSensor(entity['API'], op_code, hass.config.units, config.data["title"]))
     async_add_entities(entities)
 
 
@@ -64,6 +71,57 @@ class EchonetClimateSensor(Entity):
         else:
             return self._instance._update_data[HVAC_SENSOR_OP_CODES[self._op_code][CONF_NAME]]
         return None
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement."""
+        return self._unit_of_measurement
+
+    async def async_update(self):
+        """Retrieve latest state."""
+        await self._instance.async_update()
+
+
+class EchonetSensor(Entity):
+    def __init__(self, instance, op_code, units: UnitSystem, name=None) -> None:
+        """Initialize the sensor."""
+        self._instance = instance
+        self._eojgc = self._instance._api.eojgc
+        self._eojcc = self._instance._api.eojcc
+        self._eojci = self._instance._api.instance
+        self._op_code = op_code
+
+        if name is None:
+            self._name = f"{EOJX_CLASS[self._eojgc][self._eojcc]}-{self._op_code}"
+        else:
+            self._name = f"{name} {EOJX_CLASS[self._eojgc][self._eojcc]}-{self._op_code}"
+        self._uid = f'{self._instance._uid}-{op_code}'
+        self._unit_of_measurement = None
+
+    @property
+    def icon(self):
+        """Icon to use in the frontend, if any."""
+        return "mdi:thermometer"
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def unique_id(self):
+         """Return a unique ID."""
+         return self._uid
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        if EPC_CODE[self._eojgc ][self._eojcc][self._op_code] in self._instance._update_data:
+            if len(self._instance._update_data[EPC_CODE[self._eojgc ][self._eojcc][self._op_code]]) < 255:
+                return self._instance._update_data[EPC_CODE[self._eojgc ][self._eojcc][self._op_code]] 
+            else:
+                return 'unavailable'
+        return 'unavailable'
 
     @property
     def unit_of_measurement(self):
