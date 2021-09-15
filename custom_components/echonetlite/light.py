7ac3cd5d733165ec61aa0a8d406db00d6aeea9a5
Scott Phillips <scotty.phillips@hotmail.com>
2021-09-15 10:27:20 +1000

    Update light.py
    Fix another syntax error

--- custom_components/echonetlite/light.py
+++ custom_components/echonetlite/light.py
@@ -150,7 +150,7 @@ class EchonetLight(LightEntity):
         mired_steps = ( self.max_mireds() - self.min_mireds() ) / float(len(self._echonet_mireds))
         
         # get the current echonet mireds
-        color_temp = self._connector._update_data[ENL_COLOR_TEMP] if ENL_COLOR_TEMP in selmired_stepsf._connector._update_data else 0
+        color_temp = self._connector._update_data[ENL_COLOR_TEMP] if ENL_COLOR_TEMP in self._connector._update_data else 0
         if color_temp in self._echonet_mireds:
             self._attr_color_temp = round( self._echonet_mireds.index(color_temp) * mired_steps ) + MIN_MIREDS
         else:
