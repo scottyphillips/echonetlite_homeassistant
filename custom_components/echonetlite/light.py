63d04ae71717d7698c9700934a6ec017454949f6
Scott Phillips <scotty.phillips@hotmail.com>
2021-09-15 10:45:57 +1000

    Bug fix light class

--- custom_components/echonetlite/light.py
+++ custom_components/echonetlite/light.py
@@ -38,7 +38,7 @@ async def async_setup_entry(hass, config_entry, async_add_devices):
     entities = []
     for entity in hass.data[DOMAIN][config_entry.entry_id]:
         if entity['instance']['eojgc'] == 0x02 and  entity['instance']['eojcc'] == 0x90 : #General Lighting
-             entities.append(EchonetLight(config_entry.title, entity['echonetlite'], hass.config.units))
+             entities.append(EchonetLight(config_entry.title, entity['echonetlite']))
     async_add_devices(entities, True)
 
 
@@ -57,7 +57,7 @@ class EchonetLight(LightEntity):
             self._support_flags = self._support_flags | COLOR_MODE_BRIGHTNESS
         if ENL_COLOR_TEMP in list(self._connector._setPropertyMap):
             self._support_flags = self._support_flags | COLOR_MODE_COLOR_TEMP
-        
+
         self._echonet_mireds = [68, 67, 66, 64, 65]  # coolest to warmest
 
     async def async_update(self):
@@ -104,18 +104,18 @@ class EchonetLight(LightEntity):
     async def async_turn_on(self, kwargs):
         """Turn on."""
         await self._connector._instance.on()
-        
+
         if ATTR_BRIGHTNESS in kwargs and self._brightness is not None:
             normalized_brightness = float(kwargs[ATTR_BRIGHTNESS]) / DEFAULT_BRIGHTNESS_SCALE
             device_brightness = round(normalized_brightness * DEVICE_SCALE)
             # Make sure the brightness is not rounded down to 0
             device_brightness = max(device_brightness, 1)
-            
+
             # send the message to the lamp
             self._connector._instance.setBrightness(device_brightness)
             self._connector._update_data[ENL_BRIGHTNESS] = device_brightness
             self._attr_brightness = kwargs[ATTR_BRIGHTNESS]
-        
+
         if ATTR_COLOR_TEMP in kwargs and self._color_temp is not None:
             # send the message to the lamp
             color_scale = (float(kwargs[ATTR_COLOR_TEMP]) - float(self.min_mireds())) / float(self.max_mireds() - self.min_mireds())
@@ -124,7 +124,7 @@ class EchonetLight(LightEntity):
             for i in range(len(self._echonet_mireds) - 1):
                 if self._echonet_mireds[i] < color_scale and color_scale < self._echonet_mireds[i+1]:
                     color_temp = self._echonet_mireds[i]
-            
+
             self._connector._instance.setColorTemperature(color_temp)
             self._connector._update_data[ENL_COLOR_TEMP] = color_temp
             self._attr_color_temp = kwargs[ATTR_COLOR_TEMP]
@@ -142,14 +142,14 @@ class EchonetLight(LightEntity):
         else:
             self._attr_brightness = 128
         return self._attr_brightness
-    
+
     @property
     def color_temp(self):
         """Return the color temperature in mired."""
 
         # calculate some helper
         mired_steps = ( self.max_mireds() - self.min_mireds() ) / float(len(self._echonet_mireds))
-        
+
         # get the current echonet mireds
         color_temp = self._connector._update_data[ENL_COLOR_TEMP] if ENL_COLOR_TEMP in self._connector._update_data else 0
         if color_temp in self._echonet_mireds:
