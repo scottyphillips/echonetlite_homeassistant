788be7f846cb2174635a15908a1b231a97991a9c
Tomasz Figa <tomasz.figa@gmail.com>
2022-02-07 16:11:01 +0900

    climate: Return None instead of "unavailable"
    "unavailable" crashes Home Assistant, because it attempts to parse it as
    a number. Instead, None is the value to return if there is no value to
    return.

--- custom_components/echonetlite/climate.py
+++ custom_components/echonetlite/climate.py
@@ -131,7 +131,7 @@ class EchonetClimate(ClimateEntity):
                     return None
                 return self._connector._update_data[ENL_HVAC_ROOM_TEMP]
             else:
-                return 'unavailable'
+                return None
         else:
             return self._connector._update_data[ENL_HVAC_SET_TEMP]
 
@@ -197,7 +197,7 @@ class EchonetClimate(ClimateEntity):
     @property
     def fan_mode(self):
         """Return the fan setting."""
-        return self._connector._update_data[ENL_FANSPEED] if ENL_FANSPEED in self._connector._update_data else "unavailable"
+        return self._connector._update_data[ENL_FANSPEED] if ENL_FANSPEED in self._connector._update_data else None
 
     @property
     def fan_modes(self):
@@ -223,7 +223,7 @@ class EchonetClimate(ClimateEntity):
     @property
     def swing_mode(self):
         """Return the swing mode setting."""
-        return self._connector._update_data[ENL_AIR_VERT] if ENL_AIR_VERT in self._connector._update_data else "unavailable"
+        return self._connector._update_data[ENL_AIR_VERT] if ENL_AIR_VERT in self._connector._update_data else None
 
     async def async_set_swing_mode(self, swing_mode):
         """Set new swing mode."""
