036b07969f8a82e5f859ddc77b4317d396a306ea
nao-pon <hypweb@gmail.com>
2022-07-17 21:02:01 +0900

    Fixed the possibility duplicate of uids of switch

--- custom_components/echonetlite/switch.py
+++ custom_components/echonetlite/switch.py
@@ -64,7 +64,7 @@ class EchonetSwitch(SwitchEntity):
         self._on_vals = [self._on_value, self._options[CONF_SERVICE_DATA][DATA_STATE_ON], hex(self._options[CONF_SERVICE_DATA][DATA_STATE_ON])[2:]]
         self._attr_name = f"{config.title} {EPC_CODE[self._connector._eojgc][self._connector._eojcc][self._code]}"
         self._attr_icon = options[CONF_ICON]
-        self._uid = f'{self._connector._uid}-{self._code}'
+        self._uid = f'{self._connector._uid}-{self._connector._eojgc}-{self._connector._eojcc}-{self._connector._eojci}-{self._code}'
         self._device_name = name
         self._should_poll = True
         self.update_option_listener()
