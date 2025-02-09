73218de0754cb04ffde103ed89ac1bfb22eef29f
nao-pon <hypweb@gmail.com>
2025-02-09 16:20:51 +0900

    Added entities for 02A5F5 data debugging

--- custom_components/echonetlite/quirks/Nichicon/all/02A5.py
+++ custom_components/echonetlite/quirks/Nichicon/all/02A5.py
@@ -14,14 +14,21 @@ from ....const import TYPE_DATA_DICT
 
 
 def _02A5F5(edt):
-    d1 = d2 = None
+    d1 = d2 = d3 = d4 = None
     try:
         d1 = _signed_int(edt[0:4])
         d2 = _signed_int(edt[4:8])
+        d3 = _signed_int(edt[8:12])
+        d4 = _signed_int(edt[12:16])
     except:
         pass
     finally:
-        return {"household_consumption": d1, "from/to_grid": d2}
+        return {
+            "household_consumption": d1,
+            "from/to_grid": d2,
+            "some_a": d3,
+            "some_b": d4,
+        }
 
 
 def _02A5F6(edt):
@@ -42,7 +49,12 @@ QUIRKS = {
             CONF_NAME: "Instantaneous amount of energy",
             CONF_TYPE: SensorDeviceClass.POWER,
             CONF_STATE_CLASS: SensorStateClass.MEASUREMENT,
-            TYPE_DATA_DICT: ["household_consumption", "from/to_grid"],
+            TYPE_DATA_DICT: [
+                "household_consumption",
+                "from/to_grid",
+                "some_a",
+                "some_b",
+            ],
         },
     },
     0xF6: {
